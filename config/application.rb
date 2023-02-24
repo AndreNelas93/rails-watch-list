6ea9d8861879159ec67080635d796d604ebb890e
Andre Nelas <andrefnelas@gmail.com>
2023-02-24 16:11:37 +0000

    compile assets

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module RailsWatchList
   class Application < Rails::Application
