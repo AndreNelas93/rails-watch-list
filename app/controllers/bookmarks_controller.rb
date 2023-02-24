f23e1e0247ee65f679366cfbbfe1d3a3dbd2f048
Andre Nelas <andrefnelas@gmail.com>
2023-02-24 15:59:54 +0000

    updated homepage

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,32 +1,34 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
-    @movies = Movie.all
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to lists_path, notice: 'Bookmark was sucessfully created.'
+      redirect_to list_path(@list)
     else
-      @movies = Movie.all
       render :new
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list), notice: 'Bookmark was sucessfully deleted.'
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def set_list
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,15 +1,7 @@
 class Bookmark < ApplicationRecord
-  belongs_to :list
   belongs_to :movie
+  belongs_to :list
 
-  validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: "has already been bookmarked in this list" }
-
-  validate :movie_and_list_presence
-
-  def movie_and_list_presence
-    if movie.blank? || list.blank?
-      errors.add(:base, "A bookmark must be linked to a movie and a list")
-    end
-  end
+  validates :comment, length: {minimum: 6}
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
